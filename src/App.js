e57a9c852c747a245fadb64aa1b2ef83f60bc685
hjizerena <hajoa100@hhu.de>
2020-12-23 16:31:23 +0100

    update menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
 import {HomePage} from "./pages/homepage/homepage.component";
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+)
 
 function App() {
-  return <HomePage></HomePage>;
+  return (
+      <div>
+        <Switch>
+          <Route path='/hats' component={HatsPage}/>
+          <Route path='/' component={HomePage}/>
+        </Switch>
+      </div>
+  )
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Component} from 'react';
-import {MenuItem} from "../menu-item/menu-item.component";
+import MenuItem from "../menu-item/menu-item.component";
 import './directory.styles.scss'
 
 export class Directory extends Component {
@@ -12,7 +12,7 @@ export class Directory extends Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
@@ -47,8 +47,8 @@ export class Directory extends Component {
   render() {
     return (
         <div className='directory-menu'>
-          {this.state.sections.map(({title, imageUrl, id}) => (
-              <MenuItem key={id} title={title} imageUrl={imageUrl}/>
+          {this.state.sections.map(({id, ...otherSectionProps}) => (
+              <MenuItem key={id} {...otherSectionProps}/>
           ))}
         </div>
     );
