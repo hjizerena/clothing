e8b2a9f351014bced988f9f3d964600334c99fa2
hjizerena <hajoa100@hhu.de>
2020-12-25 13:05:20 +0100

    $1

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,20 +2,36 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import './checkout-item.style.scss';
+import {clearItemsFromCart} from "../../redux/cart/cart.actions";
 
-const CheckoutItem = ({cartItem: {name, imageUrl, price, quantity}}) => (
-    <div className='checkout-item'>
-      <div className='image-container'>
-        <img
-            src={imageUrl}
-            alt="item"
-        />
+const CheckoutItem = ({cartItem, clearItem}) => {
+  const {name, imageUrl, price, quantity} = cartItem;
+  return (
+      <div className='checkout-item'>
+        <div className='image-container'>
+          <img
+              src={imageUrl}
+              alt="item"
+          />
+        </div>
+        <span className='name'>{name}</span>
+        <span className='quantity'>
+          <div className='arrow'>&#10094;</div>
+          <span className='value'>{quantity}</span>
+          <div className='arrow'>&#10095;</div>
+        </span>
+        <span className='price'>{price}</span>
+        <div
+            className='remove-button'
+            onClick={() => clearItem(cartItem)}
+        >&#10005;</div>
       </div>
-      <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
-      <span className='price'>{price}</span>
-      <div className='remove-button'>&#10005;</div>
-    </div>
-);
+  )
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemsFromCart(item)),
+  addItem: item => dispatch()
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
